67a2449119be19cf49c6a5f4a77654e3d9383bda
ausraurnezaite <ausra.urnezaite@gmail.com>
2022-05-12 17:43:37 +0300

    XMLParser implemented

--- consoleApp/src/main/java/consoleApp/StoreApp.java
+++ consoleApp/src/main/java/consoleApp/StoreApp.java
@@ -2,6 +2,12 @@ package consoleApp;
 
 import store.Store;
 import store.helpers.StoreHelper;
+import store.parser.XMLParser;
+
+
+import java.util.Map;
+
+import static store.parser.XMLParser.getSortTypes;
 
 public class StoreApp {
     public static void main(String[] args) {
@@ -10,5 +16,8 @@ public class StoreApp {
         helper.fillStoreRandomly();
         store.printCategories();
         System.out.println(store);
+
+        Map<String, String> sortTypesMap = XMLParser.getSortTypes();
+        System.out.printf(sortTypesMap.toString());
     }
 }
--- store/src/main/java/store/helpers/StoreHelper.java
+++ store/src/main/java/store/helpers/StoreHelper.java
@@ -63,8 +63,5 @@ public class StoreHelper {
             }
         }
         return productsToAdd;
-
     }
-
-
 }
