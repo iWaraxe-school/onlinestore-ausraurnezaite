ddf270be4b6b8eb7d847dca87ea9530789e80035
ausraurnezaite <ausra.urnezaite@gmail.com>
2022-05-17 14:23:37 +0300

    Implemented Interaction

--- consoleApp/src/main/java/consoleApp/StoreApp.java
+++ consoleApp/src/main/java/consoleApp/StoreApp.java
@@ -9,6 +9,7 @@ import store.comparators.ProductComparator;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 
 public class StoreApp {
@@ -16,14 +17,18 @@ public class StoreApp {
         Store store = new Store();
         StoreHelper helper = new StoreHelper(store);
         helper.fillStoreRandomly();
-        store.printCategories();
-        System.out.println(store);
 
-        Map<String, String> sortTypesMap = XMLParser.getSortTypes("C:\\Users\\AusraUrnezaite\\IdeaProjects\\onlinestore-ausraurnezaite\\store\\src\\main\\resources\\config.xml");
-        System.out.println("Sorting types: " + sortTypesMap.toString()+ "\n");
+        Interaction.interact(store);
 
-        store.top5();
-        store.allProductsByPrice();
-        store.printSortedStore();
+
+//        store.printCategories();
+//        System.out.println(store);
+//
+//        Map<String, String> sortTypesMap = XMLParser.getSortTypes("C:\\Users\\AusraUrnezaite\\IdeaProjects\\onlinestore-ausraurnezaite\\store\\src\\main\\resources\\config.xml");
+//        System.out.println("Sorting types: " + sortTypesMap + "\n");
+//
+//        store.top5();
+//        store.allProductsByPrice();
+//        store.printSortedStore();
     }
 }
